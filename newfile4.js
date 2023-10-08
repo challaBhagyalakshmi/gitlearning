e20060c9accceb09080b04a33b263ff87f8f9a3c
Bhagyalakshmi <bhagya568n@gmail.com>
2023-10-08 19:01:55 +0530

    fix merge conflicts

--- newfile4.js
+++ newfile4.js
@@ -1,7 +1,3 @@
 console.log("This is newfile4");
-<<<<<<< Updated upstream
 console.log("how to work stash command practise");
-console.log("learning stash save command");
-=======
-console.log("how to work stash command practise");
->>>>>>> Stashed changes
+console.log("learning stash pop command");
