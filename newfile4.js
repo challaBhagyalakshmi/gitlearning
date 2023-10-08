00cc803c767efee1f5b8698c07b396d14c1fdf10
Bhagyalakshmi <bhagya568n@gmail.com>
2023-10-08 18:33:27 +0530

    fix merge conficts in newfile4

--- newfile4.js
+++ newfile4.js
@@ -1,3 +1,7 @@
 console.log("This is newfile4");
+<<<<<<< Updated upstream
 console.log("how to work stash command practise");
 console.log("learning stash save command");
+=======
+console.log("how to work stash command practise");
+>>>>>>> Stashed changes
