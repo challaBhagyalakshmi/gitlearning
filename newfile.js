648aa446fef323e3c65b86e19525e2daefad9fb9
Bhagyalakshmi <bhagya568n@gmail.com>
2024-01-24 17:42:55 +0530

    fix merge conflicts

---
+++ newfile.js
@@ -0,0 +1,7 @@
+console.log("learning stash drop command");
+console.log("learning stash change command");
+console.log("hello world");
+console.log("learning commit modified files without stage");
+console.log("learning stash command");
+console.log("learning stash pop");
+console.log("learning stash branch");
