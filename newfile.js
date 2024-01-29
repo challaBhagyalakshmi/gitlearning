75b846bfa53b4321472b69484f31331f3dd92da7
Bhagyalakshmi <bhagya568n@gmail.com>
2024-01-29 11:46:51 +0530

    Added ssh keys

--- newfile.js
+++ newfile.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 console.log("learning stash drop command");
 console.log("learning stash change command");
 console.log("hello world");
 console.log("learning commit modified files without stage");
 console.log("learning stash command");
-=======
 console.log("stash branch");
->>>>>>> feature-branch
+console.log("Added authentication keys");
